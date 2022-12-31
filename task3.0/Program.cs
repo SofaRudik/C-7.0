cda472186814b1230bc7a82baf268eb0945ab6be
sofia <sonrud@mail.ru>
2022-12-31 21:55:56 +0300

    add task 52

---
+++ task3.0/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[]coord=Console.ReadLine().Split(" ");
+int row=int.Parse(coord[0]);
+int column=int.Parse(coord[1]);
+int[,] matrix=new int [row,column];
+InputMatrix(matrix);
+Relaese(matrix);
+
+void InputMatrix (int[,]matrix)
+{
+    for(int i=0; i<matrix.GetLength(0);i++)
+    {
+        for(int j=0; j<matrix.GetLength(1);j++)
+        {
+            matrix[i,j]= new Random().Next(-10,11);
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void Relaese (int[,]matrix)
+{ for(int i=0; i<matrix.GetLength(0);i++)
+    { double sum=0;
+        for(int j=0; j<matrix.GetLength(1);j++)
+    {
+        sum+=matrix[j,i];
+    } 
+Console.WriteLine($"Результат по {i} столбцу={sum/matrix.GetLength(1)}");}
+}
